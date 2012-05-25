cfb8e238cd19139f239654d6958e4b9c8695c31d
Clay Kramer <ckramer@gmail.com>
2012-05-25 14:10:11 -0700

    pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,7 +13,10 @@ describe PagesController do
     response.should have_selector("title",
                     :content => "Ruby on Rails Tutorial Sample App | Home")
   end
-
+  it "should have a non-blank body" do
+    get 'home'
+    response.body.should_not =~ /<body>\s*<\/body>/
+   end
   end
   
 
@@ -27,6 +30,10 @@ describe PagesController do
       response.should have_selector("title",
                       :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
+    it "should have a non-blank body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
+     end
   end
 
   describe "GET 'about'" do
@@ -39,6 +46,10 @@ describe PagesController do
      response.should have_selector("title",
                      :content => "Ruby on Rails Tutorial Sample App | About")
    end
+    it "should have a non-blank body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
+     end
   end
 
 end
